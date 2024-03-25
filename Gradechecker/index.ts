51405d406325487c850ae2bede6b7a2552aef66b
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-03-25 10:32:36 +0500

    Create index.ts

---
+++ Gradechecker/index.ts
@@ -0,0 +1,8 @@
+let Marks = Math.floor(Math.random() * 80 + 1);
+if (Marks >= 80){console.log("Congratulations! You have Passed with Grade A1+");}
+else if (Marks >= 70 && Marks <= 80){console.log("Congratulations! You have Passed with Grade A+");}
+else if (Marks >= 60 && Marks < 70){console.log("Congratulations! You have Passed with Grade B");}
+else if (Marks >= 50 && Marks < 60){console.log("Congratulations! You have Passed with Grade C");}
+else if (Marks >= 40 && Marks < 50){console.log("Congratulations! You have Passed with Grade D");}
+else if (Marks >= 33 && Marks < 40){console.log("Congratulations! You have Passed with Grade E");}
+else{console.log("You are failed")};
